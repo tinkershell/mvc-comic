5a5124e02a79491c3406612566f78f5d6a70ac81
jlutge <jlutge@mitcs.com>
2018-09-12 14:54:39 -0700

    Added detail action method

--- src/MVCComic/Controllers/ComicBooksController.cs
+++ src/MVCComic/Controllers/ComicBooksController.cs
@@ -8,5 +8,9 @@ namespace MVCComic.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public string Detail()
+        {
+            return "Hello from the comic books controller!";
+        }
     }
 }
