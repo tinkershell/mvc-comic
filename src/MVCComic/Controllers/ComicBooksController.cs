e3dc30331a7947796fe178adb69adadb5beb1578
jlutge <jlutge@mitcs.com>
2018-09-12 15:30:57 -0700

    Added Comic Bookc Deatil view

--- src/MVCComic/Controllers/ComicBooksController.cs
+++ src/MVCComic/Controllers/ComicBooksController.cs
@@ -10,11 +10,7 @@ namespace MVCComic.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Wednesday)
-            {
-                return Redirect("/");
-            }
-            return Content("Hello from the comic books controller!");
+            return View();
         }
     }
 }
